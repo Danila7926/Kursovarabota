ae9c4c121f461c8ee00c067d662e09e128fbc34c
PK303-7 <PK303-7@gmail.ru>
2024-02-05 12:52:39 +0300

    ds dct dsijv gjidkzm,

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>история игр</title>
+    <link rel="icon" href="images/картинка.png ">
+    <link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+    <header>
+        <img width="100px"height="100px"src="images/картинка.png"alt="Логотип">
+        <h1>История игр</h1>
+    </header>
+    <nav>
+        <a href="/">Фото</a>
+        <a href="/admpanel">О себе</a>
+         
+    </nav>
+<main>
+    <p class="error"></p>
+    <form>
+        <label>
+            логин
+            <input type="text" name="login">
+        </label>
+        <label>
+            пароль
+            <input type="password" name="password">
+        </label>
+        <button>Войти</button>
+</form>
+</body>
+</html>
